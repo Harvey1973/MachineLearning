a20fde3c4852fb1f7861c3f82cdc0107160dc046
Harvey1973 <HarveyJ1994@gmail.com>
2017-02-21 19:43:22 -0800

    single variable works

--- ML/machine-learning-ex1/machine-learning-ex1/ex1/computeCost.m
+++ ML/machine-learning-ex1/machine-learning-ex1/ex1/computeCost.m
@@ -13,6 +13,15 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+theta0 = theta(1,:);  % first row of theta 
+theta1 = theta(2,:);  % second row of theta
+% caculate the cost using given theta values 
+temp = 0  % variabels to store (h(x)-y)^2
+for i = 1:m 
+    temp = temp + ((theta0+theta1*X(i,2))-y(i))^2;
+endfor 
+
+J=temp/(2*m)
 
 
 
